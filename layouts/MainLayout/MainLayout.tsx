702307b77248d8578423a7796fdebed1870df7d8
Darwin Lozada <darwinlozadadev@gmail.com>
2022-01-21 16:28:09 -0400

    reduce bundle size

--- layouts/MainLayout/MainLayout.tsx
+++ layouts/MainLayout/MainLayout.tsx
@@ -1,20 +1,22 @@
 import Header from 'components/Header'
-import StackedCubes from 'components/StackedCubes'
+import dynamic from 'next/dynamic'
 import { FC } from 'react'
 
 interface Props {
   bluredBackground?: boolean
 }
 
+const DynamicStackedCubes = dynamic(() => import('../../components/StackedCubes'))
+
 const MainLayout: FC<Props> = ({ children, bluredBackground }) => {
   return (
     <div className="relative overflow-hidden bg-brandBg min-h-screen">
       <Header />
       <div className="flex absolute -top-44 -left-8 w-28">
-        <StackedCubes color="pinky" className="opacity-20" />
+        <DynamicStackedCubes color="pinky" className="opacity-20" />
       </div>
       <div className="flex absolute bottom-64 top-[28rem] right-8 w-28">
-        <StackedCubes color="pinky" className="opacity-30" />
+        <DynamicStackedCubes color="pinky" className="opacity-30" />
       </div>
       <main
         className={`relative overflow-hidden z-10 min-h-full ${
