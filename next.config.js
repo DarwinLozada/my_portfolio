5af65ab117b9099a45766dcc4b3930478b26d63b
Darwin Lozada <darwinlozadadev@gmail.com>
2022-01-24 06:44:11 -0400

    add bundle analyzer to project

--- next.config.js
+++ next.config.js
@@ -1,25 +1,27 @@
 /**
  * @type {import('next').NextConfig}
  */
-
+const withBundleAnalyzer = require('@next/bundle-analyzer')
 const nextTranslate = require('next-translate')
 
-module.exports = nextTranslate({
-  images: {
-    domains: ['media.graphcms.com'],
-  },
-  webpack(config, { dev, isServer }) {
-    // ${previousConfig...}
+module.exports = withBundleAnalyzer(
+  nextTranslate({
+    images: {
+      domains: ['media.graphcms.com'],
+    },
+    webpack(config, { dev, isServer }) {
+      // ${previousConfig...}
 
-    // Replace React with Preact only in client production build
-    if (!dev && !isServer) {
-      Object.assign(config.resolve.alias, {
-        react: 'preact/compat',
-        'react-dom/test-utils': 'preact/test-utils',
-        'react-dom': 'preact/compat',
-      })
-    }
+      // Replace React with Preact only in client production build
+      if (!dev && !isServer) {
+        Object.assign(config.resolve.alias, {
+          react: 'preact/compat',
+          'react-dom/test-utils': 'preact/test-utils',
+          'react-dom': 'preact/compat',
+        })
+      }
 
-    return config
-  },
-})
+      return config
+    },
+  })
+)
