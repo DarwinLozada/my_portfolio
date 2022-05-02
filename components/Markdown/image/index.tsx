83dab2f68a4461d736af991427893e955bf4a13c
DarwinLozada <darwinlozadadev@gmail.com>
2022-05-02 05:12:28 -0400

    add see detailed image feature

--- components/Markdown/image/index.tsx
+++ components/Markdown/image/index.tsx
@@ -1,5 +1,6 @@
-import { FC } from 'react'
+import { FC, useState } from 'react'
 import NextImage from 'next/image'
+import * as Dialog from '@radix-ui/react-dialog'
 
 interface Props {
   src: string
@@ -9,16 +10,46 @@ interface Props {
 }
 
 const Image: FC<Props> = ({ src, alt, width, height }) => {
+  const [openImage, setOpenImage] = useState(false)
+
+  const handleImage = () => {
+    setOpenImage((current) => !current)
+  }
+
   return (
-    <figure className="relative">
-      <NextImage
-        src={src}
-        layout="intrinsic"
-        alt={alt}
-        width={width}
-        height={height}
-      />
-    </figure>
+    <>
+      <figure className="relative" onClick={handleImage}>
+        <NextImage
+          src={src}
+          layout="intrinsic"
+          alt={alt}
+          width={width}
+          height={height}
+        />
+      </figure>
+      <Dialog.Root
+        open={openImage}
+        onOpenChange={(open) => {
+          setOpenImage(open)
+        }}
+      >
+        <Dialog.Portal>
+          <Dialog.Overlay className="fixed top-0 z-50 grid h-screen w-screen place-items-center overflow-y-auto bg-slate-900/80 duration-300">
+            <Dialog.Content className="mx-4">
+              <figure className="relative" onClick={handleImage}>
+                <NextImage
+                  src={src}
+                  layout="intrinsic"
+                  alt={alt}
+                  width={width}
+                  height={height}
+                />
+              </figure>
+            </Dialog.Content>
+          </Dialog.Overlay>
+        </Dialog.Portal>
+      </Dialog.Root>
+    </>
   )
 }
 
