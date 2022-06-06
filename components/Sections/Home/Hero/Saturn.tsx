a15a702116a5c2a7132a92389dee7ba7045c9de2
DarwinLozada <darwinlozadadev@gmail.com>
2022-06-06 16:38:05 -0400

    add orbit controls to planet

--- components/Sections/Home/Hero/Saturn.tsx
+++ components/Sections/Home/Hero/Saturn.tsx
@@ -1,6 +1,7 @@
 import { Canvas, useLoader } from '@react-three/fiber'
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader'
 import { motion } from 'framer-motion-3d'
+import { OrbitControls } from '@react-three/drei'
 import { FC, Suspense } from 'react'
 
 const Saturn: FC = () => {
@@ -9,13 +10,10 @@ const Saturn: FC = () => {
   return (
     <Canvas frameloop="demand">
       <ambientLight intensity={0.1} />
-      <directionalLight color="red" position={[0, 0, 5]} />
-      <motion.mesh>
-        <boxGeometry args={[2, 2, 2]} />
-        <meshStandardMaterial />
-      </motion.mesh>
+      <directionalLight color="white" position={[0, 0, 5]} />
       <Suspense fallback={<p>LOADING</p>}>
         <primitive object={gltf.scene} />
+        <OrbitControls />
       </Suspense>
     </Canvas>
   )
