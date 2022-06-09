19543c90929f9c384bae7c22a3e7a13012593821
DarwinLozada <darwinlozadadev@gmail.com>
2022-06-09 16:39:47 -0400

    (WIP) make fbx model work

--- components/Sections/Home/Hero/Saturn.tsx
+++ components/Sections/Home/Hero/Saturn.tsx
@@ -1,30 +1,32 @@
-import { Canvas, useLoader } from '@react-three/fiber'
-import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader'
+import { Canvas } from '@react-three/fiber'
 import { motion } from 'framer-motion-3d'
-import { OrbitControls } from '@react-three/drei'
+import { OrbitControls, useFBX } from '@react-three/drei'
 import { FC, Suspense } from 'react'
 
 const Saturn: FC = () => {
-  const gltf = useLoader(GLTFLoader, './models/saturn/scene.gltf')
+  const gltf = useFBX('./models/saturn/scene.fbx')
+  console.log(gltf)
 
   return (
     <Canvas
       className="h-full w-screen translate-x-[30rem] overflow-visible"
-      camera={{ zoom: 0.33, position: [-1, 2, 3] }}
+      camera={{ position: [-6, 2, 6] }}
     >
       <ambientLight intensity={0.1} />
       <directionalLight color="white" position={[0, 0, 5]} />
 
       <Suspense fallback={<p>LOADING</p>}>
         <axesHelper args={[10]} />
+        <meshStandardMaterial />
+
         <motion.primitive
-          object={gltf.scene}
-          initial={{ rotateZ: 90, rotateX: 0, translateX: 400 }}
-          // animate={{ rotateZ: 360 }}
+          object={gltf}
+          initial={{ rotateY: 0 }}
+          animate={{ rotateY: Math.PI * 2 }}
           transition={{
             type: 'tween',
             ease: 'linear',
-            duration: 2000,
+            duration: 5,
             repeat: Infinity,
             repeatType: 'loop'
           }}
