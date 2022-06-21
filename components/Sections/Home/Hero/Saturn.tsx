5d95ab549c80c75c48cb70c13edbff5dfb1437a1
DarwinLozada <darwinlozadadev@gmail.com>
2022-06-21 13:58:45 -0400

    adjust position and controls of planet

--- components/Sections/Home/Hero/Saturn.tsx
+++ components/Sections/Home/Hero/Saturn.tsx
@@ -4,6 +4,7 @@ import { OrbitControls, useFBX, PerspectiveCamera } from '@react-three/drei'
 import { FC, Suspense, useRef } from 'react'
 import type { Camera } from 'three'
 import { TextureLoader } from 'three/src/loaders/TextureLoader'
+import { m } from 'framer-motion'
 
 const Scene: FC = () => {
   const planet = useFBX('./models/saturn/planet.fbx')
@@ -23,11 +24,10 @@ const Scene: FC = () => {
       <Suspense fallback={<p>LOADING</p>}>
         <>
           <axesHelper args={[10]} />
-
           <motion.primitive
             object={planet}
             scale={[0.0004, 0.0004, 0.0004]}
-            initial={{ rotateY: 0, rotateX: Math.PI / 5 }}
+            initial={{ rotateY: 0, rotateX: Math.PI / 7 }}
             animate={{ rotateY: Math.PI * 2 }}
             transition={{
               type: 'tween',
@@ -41,7 +41,7 @@ const Scene: FC = () => {
           <motion.primitive
             object={asteroidsOne}
             scale={[0.0004, 0.0004, 0.0004]}
-            initial={{ rotateY: 0, rotateX: Math.PI / 5 }}
+            initial={{ rotateY: 0, rotateX: Math.PI / 7 }}
             animate={{ rotateY: Math.PI * 2 }}
             transition={{
               type: 'tween',
@@ -55,7 +55,7 @@ const Scene: FC = () => {
           <motion.primitive
             object={asteroidsTwo}
             scale={[0.00045, 0.00045, 0.00045]}
-            initial={{ rotateY: 0, rotateX: Math.PI / 5 }}
+            initial={{ rotateY: 0, rotateX: Math.PI / 7 }}
             animate={{ rotateY: Math.PI * 2 }}
             transition={{
               type: 'tween',
@@ -68,10 +68,11 @@ const Scene: FC = () => {
 
           <meshStandardMaterial map={texture} />
 
-          <PerspectiveCamera ref={cameraRef} position={[0, 0, 5]} />
+          <PerspectiveCamera ref={cameraRef} />
           <OrbitControls
             camera={cameraRef.current}
-            // onChange={(state) => console.log(state.target)}
+            maxPolarAngle={Math.PI / 2}
+            onChange={(state) => console.log(state.target)}
           />
         </>
       </Suspense>
@@ -83,7 +84,7 @@ const Saturn: FC = () => {
   return (
     <Canvas
       className="h-full w-screen translate-x-[30rem] overflow-visible"
-      camera={{ position: [-6, 2, 6] }}
+      camera={{ position: [-5.49, -3.38, 11.6] }}
     >
       <Scene />
     </Canvas>
