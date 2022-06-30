fc394714816d3a948052b398bfdce1dfce94c1e0
DarwinLozada <darwinlozadadev@gmail.com>
2022-06-30 18:37:35 -0400

    add more planets

--- components/Sections/Home/Hero/Saturn.tsx
+++ components/Sections/Home/Hero/Saturn.tsx
@@ -1,42 +1,103 @@
-import { Canvas, useLoader } from '@react-three/fiber'
+import { Canvas, useFrame, useLoader } from '@react-three/fiber'
 import { motion } from 'framer-motion-3d'
 import { OrbitControls, useFBX, PerspectiveCamera } from '@react-three/drei'
 import { FC, memo, Suspense, useEffect, useRef } from 'react'
-import { Camera, Mesh, MeshStandardMaterial } from 'three'
+import { Camera, Group, Mesh, MeshStandardMaterial } from 'three'
 
 import { TextureLoader } from 'three/src/loaders/TextureLoader'
 import useIsClient from 'hooks/useIsClient'
+const DISTANCE_TO_SUN = 10
 
 const Scene: FC = () => {
-  const planet = useFBX('./models/saturn/planet.fbx')
+  const saturn = useFBX('./models/saturn/planet.fbx')
   const asteroidsOne = useFBX('./models/saturn/asteroids1.fbx')
   const asteroidsTwo = useFBX('./models/saturn/asteroids2.fbx')
+
+  const lavaPlanet1 = useFBX('./models/extra_planets/lava_planet1.fbx')
+  const sun = useFBX('./models/extra_planets/sun.fbx')
+
+  const planetsGroupRef = useRef<Group | null>(null)
 
   const texture = useLoader(TextureLoader, './models/saturn/textures.png')
 
   const cameraRef = useRef<Camera>()
 
   useEffect(() => {
-    planet.children.forEach((child) => {
+    saturn.children.forEach((child) => {
       if (child instanceof Mesh) {
         child.material = new MeshStandardMaterial({
           map: texture,
         })
       }
     })
-  }, [planet, texture])
+
+    lavaPlanet1.children.forEach((child) => {
+      if (child instanceof Mesh) {
+        child.material = new MeshStandardMaterial({
+          map: texture,
+        })
+      }
+    })
+
+    sun.children.forEach((child) => {
+      if (child instanceof Mesh) {
+        child.material = new MeshStandardMaterial({
+          map: texture,
+        })
+      }
+    })
+
+    planetsGroupRef.current?.position.set(-3, 0, -50)
+  }, [saturn, texture, lavaPlanet1, sun])
+
+  useFrame(() => {
+    const date = Date.now() * 0.0001
+
+    lavaPlanet1.position.x = Math.cos(date) * DISTANCE_TO_SUN
+    lavaPlanet1.position.z = Math.sin(date) * DISTANCE_TO_SUN
+  })
 
   return (
     <>
       <ambientLight intensity={0.07} />
       <directionalLight color="white" position={[0, 0, 5]} />
+      <axesHelper args={[20]} />
 
       <Suspense fallback={<p>LOADING</p>}>
         <>
+          <group ref={planetsGroupRef}>
+            <motion.primitive
+              object={lavaPlanet1}
+              scale={[0.0002, 0.0002, 0.0002]}
+              initial={{ y: 4, x: -5, z: -50 }}
+              animate={{ rotateY: Math.PI * 2 }}
+              transition={{
+                type: 'tween',
+                ease: 'linear',
+                duration: 30,
+                repeat: Infinity,
+                repeatType: 'loop',
+              }}
+            />
+
+            <motion.primitive
+              object={sun}
+              scale={[0.0003, 0.0003, 0.0003]}
+              initial={{ y: 4, x: 0, z: 0 }}
+              animate={{ rotateY: Math.PI * 2 }}
+              transition={{
+                type: 'tween',
+                ease: 'linear',
+                duration: 30,
+                repeat: Infinity,
+                repeatType: 'loop',
+              }}
+            />
+          </group>
+
           <motion.primitive
-            object={planet}
+            object={saturn}
             scale={[0.0004, 0.0004, 0.0004]}
-            position={[0, 4, 0]}
             initial={{ rotateY: 0, rotateX: Math.PI / 7, y: 3 }}
             animate={{ rotateY: Math.PI * 2 }}
             transition={{
@@ -46,9 +107,7 @@ const Scene: FC = () => {
               repeat: Infinity,
               repeatType: 'loop',
             }}
-          >
-            <meshStandardMaterial color="hotpink" />
-          </motion.primitive>
+          />
 
           <motion.primitive
             object={asteroidsOne}
@@ -76,9 +135,7 @@ const Scene: FC = () => {
               repeat: Infinity,
               repeatType: 'loop',
             }}
-          >
-            <meshStandardMaterial color="hotpink" />
-          </motion.primitive>
+          />
 
           <PerspectiveCamera ref={cameraRef} />
           <OrbitControls
@@ -97,12 +154,12 @@ const Saturn: FC = () => {
   const hasMounted = useIsClient()
 
   return (
-    <section className="absolute top-0 flex h-[calc(100vh+20rem)] w-[80rem] grow items-center justify-center overflow-visible">
+    <section className="absolute top-0 flex h-[calc(100vh+20rem)] w-[calc(100%*2)] grow items-center justify-center overflow-visible">
       <Canvas
-        className={`relative w-[70rem] translate-x-[9rem] translate-y-[27rem] scale-75 overflow-visible ${
+        className={`relative w-[calc(100%*2)] translate-x-[9rem] translate-y-[27rem] scale-75 overflow-visible ${
           hasMounted ? 'opacity-80 md:opacity-100' : 'opacity-0'
         }  transition-opacity duration-500 md:translate-x-[7rem] md:translate-y-[13rem]  md:scale-100 
-      lg:translate-y-0 lg:translate-x-[40vw]`}
+      lg:translate-y-0 lg:translate-x-[-10vw]`}
         camera={{ position: [-12.49, 3.54, 8.53] }}
       >
         <Scene />
